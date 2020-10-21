82ce4cca2ab1b2ab3d79124f3868f54865f80f5e
21toffy <oketofoke@gmail.com>
2020-10-21 23:13:50 +0100

    alter file systems

--- app.js
+++ app.js
@@ -1,42 +1,72 @@
 // working with readline module
 
-const readline  = require('readline');
-const rl =  readline.createInterface({
-
-    input: process.stdin,
-    output: process.stdout  
-
-});
 
 
-let num1 = Math.floor(Math.random() * 10) + 1;
 
-let num2 = Math.floor(Math.random() * 10) + 1;
-let answer = num1 + num2;
-rl.question(`what is ${num1} + ${num2}? \n`, 
-    (userInput)  =>{
-        if(userInput.trim()==answer){
-            rl.close();
-        }
-        else{
-            rl.setPrompt('incorrect input!! \n');
-            rl.prompt();
-            rl.on('line', (userInput)=>{
-                if(userInput==answer){
-                    rl.close();
-                }
-                else{
-                    rl.setPrompt(`your answer of ${ userInput } is wrong try again !! \n`);
-                    rl.prompt();
-                }
-            });
+const fs = require('fs');
 
-        }
-    });
+// fs.rename('text.txt', 'example.txt', (err)=>{
+//     if (err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log('successfully renamed the file!!!!')
+//     }
+// })
+// fs.writeFile('text.txt', 'sample ekwe!!!!!!!!!!!!',(err=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log('file successfully altered');
+//         fs.readFile('text.txt','utf8',(err, file)=>{
+//             if(err){
+//                 console.log(err);
+//             }
+//             else{
+//                 console.log(file);
+//             }
+//         })
+//     }
+// }))
 
-rl.on('close', ()=>{
-    console.log('closed !!!!!!!')
-});
+// const readline  = require('readline');
+// const rl =  readline.createInterface({
+
+//     input: process.stdin,
+//     output: process.stdout  
+
+// });
+
+
+// let num1 = Math.floor(Math.random() * 10) + 1;
+
+// let num2 = Math.floor(Math.random() * 10) + 1;
+// let answer = num1 + num2;
+// rl.question(`what is ${num1} + ${num2}? \n`, 
+//     (userInput)  =>{
+//         if(userInput.trim()==answer){
+//             rl.close();
+//         }
+//         else{
+//             rl.setPrompt('incorrect input!! \n');
+//             rl.prompt();
+//             rl.on('line', (userInput)=>{
+//                 if(userInput==answer){
+//                     rl.close();
+//                 }
+//                 else{
+//                     rl.setPrompt(`your answer of ${ userInput } is wrong try again !! \n`);
+//                     rl.prompt();
+//                 }
+//             });
+
+//         }
+//     });
+
+// rl.on('close', ()=>{
+//     console.log('closed !!!!!!!')
+// });
 
 // Events Module
 // const EventEmitter = require('events')
